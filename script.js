96ca7d9460ab136ab9470a85623aa7c23c93543d
VR <nanocontroller@gmail.com>
2022-08-01 17:28:28 -0700

    Full update to new specifications

---
+++ script.js
@@ -0,0 +1,141 @@
+const DECADE_CONTAINER = document.getElementById('decades-container')
+const MOBILE_DA = document.getElementById('decades-container')
+const DATA_CONTAINER = document.getElementById('data-container')
+const OVERLAY = document.getElementById('overlay')
+
+const popupData = []
+
+const handleOnload = async () => {
+  let data = await fetch('data.json').then((res) => res.json())
+  data.map((item, i) => {
+    let LetItem = item;
+    // console.log(LetItem)
+
+    if (item.year % 10 === 0) {
+      let decadeButton = document.createElement('a')
+      console.log(decadeButton)
+
+      decadeButton.innerHTML = item.year
+      decadeButton.className = 'decade-button'
+      decadeButton.href = `#section-${item.year}`
+      decadeButton.onclick = (e) =>  handleDecadeClick(e)
+      
+      DECADE_CONTAINER.appendChild(decadeButton)
+    }
+    createData(item, i)
+
+  })
+}
+
+const handleDecadeClick = (decade) => {
+// console.log(decade.target.item)
+
+}
+
+const createData = (item, i) => {
+   if (item.year % 10 === 0) {
+    let decateIntro = `
+    <div class='decade-intro' id=section-${item.year}>
+      <div class='decade-intro-main' >
+        <h1><span class="circle"></span>${item.year + 's'}</h1>
+        <img class='decade-intro-img' src='https://raw.github.com/holihollyday/image_HistoryofComputing/main/${item.featureimg}'/>
+      </div>
+      <p class='decade-intro-p'>${item.feature}</p>
+    </div>
+    `
+    DATA_CONTAINER.innerHTML += decateIntro
+  }
+  let decadeYearData = `
+    <div class='decade-data'>
+      <div class='decade-main'>
+      <h2>${item.year}</h2>
+      <img class='decade-img' src='https://raw.github.com/holihollyday/image_HistoryofComputing/main/${item.featureimg}'/>
+      <p>${item.feature}</p>
+      ${renderButtons(item, i)}
+      </div>
+    </div>
+  `
+  DATA_CONTAINER.innerHTML += decadeYearData
+}
+const renderDiv = (title, image) => {
+  return `
+  <div class='popup-box'>
+    <h3>${title}</h3>
+    <img class='popup-img' src='https://raw.github.com/holihollyday/image_HistoryofComputing/main/${image}'/>
+  </div>
+  `
+}
+const renderButtons = (data, i) => {
+  // console.log(data.year)
+  let popup = `<div>`
+  if (data.event1 !== '') {
+    popup += `
+    <div>
+      <h1>${data.year}</h1>
+      <h3>Events</h3>
+      ${renderDiv(data.event1, data.event1img)}
+      ${data.event2 !== '' ? renderDiv(data.event2, data.event2img) : ''}
+    </div>
+    `
+  }
+
+  if (data.software1 !== '') {
+    popup += `
+    <div>
+    <h3>software</h3>
+    ${renderDiv(data.software1, data.software1img)}
+    ${data.software2 !== '' ? renderDiv(data.software2, data.software2img) : ''}
+  </div>
+    `
+  }
+
+  if (data.system1 !== '') {
+    popup += `
+    <div>
+    <h3>software</h3>
+    ${renderDiv(data.system1, data.system1img)}
+    ${data.system2 !== '' ? renderDiv(data.system2, data.system2img) : ''}
+    ${data.system3 !== '' ? renderDiv(data.system3, data.system3img) : ''}
+    ${data.system4 !== '' ? renderDiv(data.system4, data.system4img) : ''}
+    ${data.system5 !== '' ? renderDiv(data.system5, data.system5img) : ''}
+  </div>
+    `
+  }
+  if (data.person1 !== '') {
+    popup += `
+    <div>
+    <h3>Person</h3>
+    ${renderDiv(data.person1, data.person1img)}
+    ${data.person2 !== '' ? renderDiv(data.person2, data.person2img) : ''}
+  </div>
+    `
+  }
+  popup += '</div>'
+  popupData.push(popup)
+  if (data.event1 !== '' || data.software1 !== '' || data.system1 !== '' || data.person1 !== '') {
+    return `<div onclick='showPopup(${i})' class="read-more-btn"><span class="read-more-icon"></span>Read more</div>`
+  } else {
+    return ''
+  }
+}
+
+const showPopup = (i) => {
+  let popupContainer = document.getElementById('popup-container');
+  document.getElementById('popup-data').innerHTML = popupData[i];
+  OVERLAY.style.display = 'block'
+  popupContainer.style.display = 'block'
+}
+
+const hidePopup = () => {
+  let popupContainer = document.getElementById('popup-container');
+  popupContainer.style.display = 'none'
+  document.getElementById('popup-data').innerHTML = ''
+  OVERLAY.style.display = 'none'
+
+
+}
+
+DATA_CONTAINER.addEventListener('wheel', (evt) => {
+  evt.preventDefault()
+  DATA_CONTAINER.scrollLeft += evt.deltaY
+})
---
+++ select.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector(".wrapper"),
+  selectBtn = wrapper.querySelector(".select-btn"),
+  options = wrapper.querySelector(".options")
+async function addDecade(selectedDecade) {
+  let data = await fetch("data.json").then((res) => res.json())
+  let decades = data.map((item, i) => {
+    if (item.year % 10 === 0) {
+      return item.year
+    }
+  })
+  options.innerHTML = ""
+  decades.forEach((decade) => {
+    let isSelected = decade == selectedDecade ? "selected" : ""
+    if (decade !== undefined) {
+      let li = `<li  onclick="updateName(this)" class="${isSelected}"><a href=#section-${decade}>${decade}</a></li>`
+      options.insertAdjacentHTML("beforeend", li)
+    }
+  })
+}
+addDecade()
+
+function updateName(selectedLi) {
+  addDecade(selectedLi.innerText)
+  wrapper.classList.remove("active")
+  selectBtn.firstElementChild.innerText = selectedLi.innerText
+}
+
+selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"))
